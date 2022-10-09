105de277b627c6f6b5488a6d547e11a46a3fa9cd
David Tang <687449+iamdtang@users.noreply.github.com>
2022-10-08 21:32:11 -0700

    Week 8.5 - Toast Notifications

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../Navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Root() {
   return (
     <div className="container">
       <Navigation />
       <Outlet />
+      <ToastContainer />
     </div>
   );
 }
